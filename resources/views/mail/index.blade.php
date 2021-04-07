81c2b88d2be97c8444c6acd58700cf3bce4e40e9
o1432257 <72015227+o1432257@users.noreply.github.com>
2021-04-07 15:27:58 +0800

    Revert "Revert "修改信件""
    This reverts commit 158f28e06d29c400c3ccdc2882b990863f254fba.

--- resources/views/mail/index.blade.php
+++ resources/views/mail/index.blade.php
@@ -1,1 +1,14 @@
-Hello
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<p>{{$admin->name}}</p>
+<p>{{$admin->email}}</p>
+<a href="https://www.google.com/">設置新密碼</a>
+</body>
+</html>
